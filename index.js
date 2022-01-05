ae56ade8e3dd220aa534a61fce3f2481fb679906
adlinahmed <59899886+adlinahmed@users.noreply.github.com>
2022-01-04 21:28:35 -0600

    added login functionality

--- index.js
+++ index.js
@@ -1,59 +1,97 @@
 const { request } = require("express");
 const express = require("express");
+const session = require("express-session");
+const req = require("express/lib/request");
 const res = require("express/lib/response");
 const app = express();
 const port = process.env.PORT || 2900;
 
-app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json()); 
+
+app.use(session({
+    secret: 'random string',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+  }))
 
 app.set("view engine", "ejs");
 
-app.get("/", (request,response) => {
-    response.render("welcome");
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", (req,res) => {
+    let user = "";
+    if (req.session && req.session.username) {
+        user = req.session.username;
+    } 
+    res.render("index", {my_user : user});
 }) 
+app.post("/signup", (req,res) => {
+    const validUsers = ["adlin", "miso"];
+    const user = req.body.username;
+    if (validUsers.includes(user)) {
+        req.session.username = user;
+        res.redirect("/welcome");
+    } else {
+        req.session.destroy(()=>{
+            console.log("user reset");
+        })
+        res.redirect("/");
+    }
+    // req.session.username = user;
+    // res.redirect("/welcome/"); 
 
-app.get("/welcome/", (request,response) => {
-    response.render("welcome");
+}) 
+app.get("/welcome/", (req,res) => {
+    // const user = req.body.username;
+
+    if (req.session && req.session.username) {
+        // res.send(`Welcome ${user}`);
+        res.render("welcome", {user: req.session.username}); 
+    } else {
+        res.redirect("/");
+    }
 })
 
-app.get("/room1/", (request,response) => {
-    response.render("room1");
+app.get("/room1/", (req,res) => {
+    res.render("room1");
 })
 
-app.get("/room2/", (request,response) => {
-    response.render("room2");
+app.get("/room2/", (req,res) => {
+    res.render("room2");
 })
 
-app.get("/room3/", (request,response) => {
-    response.render("room3");
+app.get("/room3/", (req,res) => {
+    res.render("room3");
 })
 
-app.get("/room4/", (request,response) => {
-    response.render("room4");
+app.get("/room4/", (req,res) => {
+    res.render("room4");
 })
 
-app.get("/room5/", (request,response) => {
-    response.render("room5");
+app.get("/room5/", (req,res) => {
+    res.render("room5");
 })
 
-app.get("/room6/", (request,response) => {
-    response.render("room6");
+app.get("/room6/", (req,res) => {
+    res.render("room6");
 })
 
-app.get("/room7/", (request,response) => {
-    response.render("room7");
+app.get("/room7/", (req,res) => {
+    res.render("room7");
 })
 
-app.get("/room8/", (request,response) => {
-    response.render("room8");
+app.get("/room8/", (req,res) => {
+    res.render("room8");
 })
 
-app.get("/lose/", (request,response) => {
-    response.render("lose");
+app.get("/lose/", (req,res) => {
+    res.render("lose");
 })
 
-app.get("/win/", (request,response) => {
-    response.render("win");
+app.get("/win/", (req,res) => {
+    res.render("win");
 })
 
 app.listen(port, ()=> {
